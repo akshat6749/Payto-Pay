8abbadf279904b7229c058d474c05b58dfba58d4
Akshat Jaiswal <akshatjaiswal872@gmail.com>
2026-04-28 00:55:30 +0530

    chore: remove unwanted test files

--- test_final.py
+++
@@ -1,7 +0,0 @@
-﻿import os, sys, time, uuid
-os.environ['DJANGO_SETTINGS_MODULE']='payout_engine.settings'
-import django; django.setup()
-from payout_engine.core.models import Payout
-
-payout = Payout.objects.order_by('-created_at').first()
-print('FINAL PAYOUT STATUS:', payout.status)
